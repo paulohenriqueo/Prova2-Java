fb2b196e4f71aa82ecf19c56d550bcd7bb4a283c
paulohenriqueo <paulohenrique300305@gmail.com>
2024-06-25 10:06:21 -0300

    :sparkles: feat: end point PUT/cities

--- src/main/java/com/fatec/city/resources/CityController.java
+++ src/main/java/com/fatec/city/resources/CityController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,10 @@ public class CityController {
     @GetMapping("{id}")
     public City geCityById(@PathVariable int id){
         return cityService.getCityById(id);
+    }
+
+    @PutMapping("{id}")
+    public void uptade(@PathVariable int id, @RequestBody City city){
+        this.cityService.update(id, city);
     }
 }
--- src/main/java/com/fatec/city/resources/services/CityService.java
+++ src/main/java/com/fatec/city/resources/services/CityService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import com.fatec.city.dto.CityResponse;
 import com.fatec.city.entities.City;
 import com.fatec.city.mappers.CityMapper;
@@ -28,11 +29,22 @@ public class CityService {
                                 .collect(Collectors.toList());
     }
 
-    
     public City getCityById(int id){
         return cityRepository.findById(id).orElseThrow(
             () -> new EntityNotFoundException("Cidade não encontrada")
         );
     }
 
+    public void update (int id, City city){
+        try{
+            City aux = cityRepository.getReferenceById(id);
+            aux.setName(city.getName());
+            aux.setEstado(city.getEstado());
+            aux.setPopul(city.getPopul());
+            aux.setPib(city.getPib());
+            this.cityRepository.save(aux);
+        } catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException("Cidade não cencontrada");
+        }
+    }
 }

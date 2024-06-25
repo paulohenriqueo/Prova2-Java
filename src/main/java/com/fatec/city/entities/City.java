22bca2a3deb80084effeee62535278606512aa79
paulohenriqueo <paulohenrique300305@gmail.com>
2024-06-25 08:37:18 -0300

    :sparkles: feat: mapeamento com banco de dados

--- src/main/java/com/fatec/city/entities/City.java
+++ src/main/java/com/fatec/city/entities/City.java
@@ -1,9 +1,22 @@
 package com.fatec.city.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TBL_CITY")
 public class City {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @Column
     private String name;
+
     private String estado;
     private String popul;
     private String pib;

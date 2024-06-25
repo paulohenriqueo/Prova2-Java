f8f7b48029710f9f7121c0134deb5b95272647bc
paulohenriqueo <paulohenrique300305@gmail.com>
2024-06-25 09:06:08 -0300

    :sparkles: feat: DTO response e request

---
+++ src/main/java/com/fatec/city/dto/CityRequest.java
@@ -0,0 +1,5 @@
+package com.fatec.city.dto;
+
+public record CityRequest (String name, String estado, String popul, String pib) {
+    
+}
---
+++ src/main/java/com/fatec/city/dto/CityResponse.java
@@ -0,0 +1,5 @@
+package com.fatec.city.dto;
+
+public record CityResponse( int id, String name, String estado, String popul, String pib) {
+    
+}
